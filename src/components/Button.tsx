8c872d0130a0ca000b424c857118efa87bea0a4b
Vika <vikapetrova8@gmail.com>
2024-09-12 22:40:20 +0300

    Button

---
+++ src/components/Button.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+type ButtonPropsType = {
+    title: string
+    callBack: () => void
+}
+
+export const Button = (props: ButtonPropsType) => {
+    const onClickButtonHandler = () => {
+        props.callBack()
+    }
+
+    return (
+        <button onClick={onClickButtonHandler}>{props.title}</button>
+    );
+};
